292b7efc652ec383cf263c6c1846f0a4a61e1b0f
Vasiliy Alekseev <alekseev.v007@yandex.ru>
2024-03-24 18:04:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10_000;
+        int priceMiles = 20;
+        int bonusMiles = ticketPrice / priceMiles;
+
+        System.out.println(bonusMiles);
+
+    }
+}
